2811dca018c90748d29997dad953b45dff3c9e8c
NitroSkate <00159817@uca.edu.sv>
2022-04-21 20:24:57 -0600

    Cambios UI

---
+++ resources/views/formulariosDoc/menuprincipal.blade.php
@@ -0,0 +1,50 @@
+@extends('plantillas.maintemplate')
+@section('content')
+<!--@push('styles')
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+@endpush-->
+    <div class="container-fluid">
+        <div class="row">
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, quos?</h1>
+        </div> 
+        <div class="row">
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat molestiae eveniet ullam nostrum earum possimus.</h2>
+        </div> 
+        <div class="row">
+            <div class="container" style="background-color: silver; width: 100%;">
+                <div class="row row-cols-1">
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="img/LogoUCAnegro.png" class="card-img-top">
+                            <div class="card-body" style="background-color: skyblue">
+                                <h5 class="card-title">Portada</h5>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="img/LogoUCAnegro.png" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Portada</h5>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="img/LogoUCAnegro.png" class="card-img-top">
+                            <div class="card-body">
+                                <h5 class="card-title">Portada</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        
+        <div class="row">
+            <div>
+            </div>
+        </div> 
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('plantillas.maintemplate');
+});
+
+Route::get('/menu', function () {
+    return view('formulariosDoc.menuprincipal');
 });
 
 
