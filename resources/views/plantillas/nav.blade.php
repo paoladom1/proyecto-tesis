cd5f9dcdd46ee360780371a544f7250a8ef58ec2
NitroSkate <00159817@uca.edu.sv>
2022-05-03 09:18:33 -0600

    Fix de navbar y sidebar

--- resources/views/plantillas/nav.blade.php
+++ resources/views/plantillas/nav.blade.php
@@ -30,13 +30,106 @@
 
     <title>Proceso de graduación</title>
 </head>
-<body>
+<style>
 
-    <nav class="navGral" id="nav">
+#user-btn{
+    margin-right: 1em;
+    background-color: #003C71;
+    border: none;
+    transition: all 0.3s;
+}
+
+#profile{
+    transition: all 0.3s;
+}
+
+#user-btn:hover #profile{
+    transform: scale(1.15);
+}
+#triangulo{
+    width: 0;
+    height: 0;
+    border-top: 8px solid white;
+    border-left: 4px solid transparent;
+    border-right: 4px solid transparent;
+    align-self: center;
+}
+.user-content{
+    display: none;
+    position: absolute;
+    background-color: #f1f1f1;
+    width: 4%;
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    z-index: 1;
+}
+
+.user-content a {
+  color: black;
+  text-decoration: none;
+    padding: 1em;
+  display: block;
+  font-size: 0.85em;
+}
+.user-content a:hover {
+    background-color: white;
+}
+
+.show {display:block;}
+
+#accordion{
+    transition: all 0.5s;
+}
+
+#accordion:hover{
+    background-color: skyblue;
+}
+
+.dropdown-btn{
+    background-color: skyblue;
+    border: none;
+    color: white;
+}
+
+ul li a:hover {
+    color: #fff;
+    background: #E87B2A
+}
+ul li.active>a,
+a[aria-expanded="true"] {
+    color: #fff;
+    background: #E87B2A
+}
+
+</style>
+
+
+<body>
+    <nav class="navGral d-flex justify-content-between" id="nav">
         <a href="#">
             <img src="/img/Logo.png" alt="" class="d-inline-block align-text-top navImg">
+           
         </a>
-        <p style="color: white;" class="navTitle align-items-center">Tema de tesis <!--Variable laravel--></p>
+        <div class="dropdown" id="user-settings">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="user-btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="img/profile.png" id="profile" style="width: 2.5em; border-radius: 100%;"/>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="user-btn">
+              <a class="dropdown-item" href="#">Editar perfil</a>
+              <hr class="dropdown-divider">
+              <a class="dropdown-item" href="#">Cerrar sesion</a>
+            </div>
+          </div>
+        <!--<div id="user-settings">
+            <button id="user-btn" onclick="dropdown()">
+                <img src="img/profile.png" id="profile" style="width: 2.5em; border-radius: 100%;"/>
+                <div id="triangulo" class="ms-2">
+                </div>
+            </button>
+            <div id="user-dropdown" class="user-content">
+                <a style="width: 100%;" href="#">Editar perfil</a>
+                <a style="width: 100%;" class="ucontent" href="#">Cerrar sesion</a>
+              </div>
+        </div>-->
     </nav>
 
      <!-- Menu desplegable lateral -->
@@ -46,7 +139,7 @@
                 <a href="javascript:void(0)" class="btn-close btn-close-white" onclick="closeNav()"></a>
             </div>
 
-            <div class="profileItem row">
+            <!--<div class="profileItem row">
                 <div class = "col">
                     <img class="profileImg" src="/img/profile.png" alt="" class="d-inline-block align-text-top">
                 </div>
@@ -58,32 +151,40 @@
                         <p>Grupo x</p>
                     </div>
                 </div>
-            </div>
+            </div>-->
             <hr>
-                <!--<div class="dropdown opMenu">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-                        <li><a class="dropdown-item active" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Separated link</a></li>
-                    </ul>
-                </div>-->
-                <div class="accordion accordion-flush" id="accordionFlushExample">
-                    <div class="accordion-item">
-                      <h2 class="accordion-header" id="flush-headingOne">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                          Accordion Item #1
-                        </button>
-                      </h2>
-                      <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
-                      </div>
+            <!--<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                  <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                      <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Portadas
+                      </a>
+                    </h4>
+                  </div>
+                  <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                      <ul>
+                        <a><button class="dropdown-btn">Primer Portada</button></a>
+                        <a><button class="dropdown-btn">Segunda Portada</button></a>
+                      </ul>
                     </div>
                   </div>
+                </div>
+              </div>-->
+              <ul class="list-unstyled components">
+                <li> <a href="#portadas" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Portada</a>
+                    <ul class="collapse list-unstyled" id="portadas">
+                        <li> <a href="#">Primer Portada</a> </li>
+                        <li> <a href="#">Segunda Portada</a> </li>
+                    </ul>
+                </li>
+                <li> <a href="#">Timeline</a> </li>
+                <li> <a href="#">Live Chat</a> </li>
+                <li> <a href="#">Likes</a> </li>
+                <li> <a href="#">Comments</a> </li>
+            </ul>
+            
             <hr>
             <div class="logOut">
                 <a href="#">Cerrar sesión</a>
@@ -109,13 +210,16 @@
             document.getElementById("content").style.marginLeft = "250px";
             //document.getElementById("menuB").style.marginLeft = "250px";
             document.getElementById("menu").style.display = 'none';
+            document.getElementById("user-settings").style.marginRight = "15em";
+            document.getElementById("user-settings").style.transition = "all 0.5s";
         }
         function closeNav(){
             document.getElementById("SideNav").style.marginLeft = "-250px";
             document.getElementById("nav").style.marginLeft = "0";
             document.getElementById("content").style.marginLeft = "0";
             document.getElementById("menu").style.display = 'initial';
-            document.getElementById("navMenu").style.fontSize="100%"
+            document.getElementById("navMenu").style.fontSize="100%";
+            document.getElementById("user-settings").style.marginRight = "0.25em";
         }
 
         document.getElementById("menu").addEventListener("click", toggleNav);
@@ -126,9 +230,8 @@
             return openNav();
         }
 
-        var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-        var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-        return new bootstrap.Dropdown(dropdownToggleEl)
-        })
+        function dropdown() {
+            document.getElementById("user-dropdown").classList.toggle("show");
+            }
     </script>
 @include('plantillas.footer')
