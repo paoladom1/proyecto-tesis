46a4cac80a874e5122e189f45b66a93204abb86e
Eduardo-00092117 <37190875+Eduardo-00092117@users.noreply.github.com>
2022-06-01 23:44:25 -0600

    corrección de rutas

--- routes/web.php
+++ routes/web.php
@@ -77,11 +77,12 @@ Route::post('/guardarAgradecimiento', 'App\Http\Controllers\EstudianteController
 Route::post('/guardarDedicatoria', 'App\Http\Controllers\EstudianteController@saveDedicatoria');
 Route::post('/cambiarEstado', 'App\Http\Controllers\EstudianteController@cambioEstado');
 
+// Director de carrera
+Route::get('/menudirector', "App\Http\Controllers\DirectorController@menuDirector");
+
 Route::get('/perfil', function () {
     return view('usuarios.usuario');
 });
-
-Route::get('/menudirector', "App\Http\Controllers\DirectorController@menuDirector");
 
 Route::get('/lector', function () {
     return view('director.lector');
