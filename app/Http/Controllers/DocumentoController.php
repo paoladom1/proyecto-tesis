f76ecc9778c57baf294f61dc74586aa3bf1bec5a
Hanyauku123 <00353817@uca.edu.sv>
2023-05-24 01:59:01 -0600

    agregando comentarios a código herramienta texto

--- app/Http/Controllers/DocumentoController.php
+++ app/Http/Controllers/DocumentoController.php
@@ -379,11 +379,7 @@ class DocumentoController extends Controller
             if($documentoTodo == null){
                 $section = $documento->addSection(DocumentoController::margenes());
             } else{
-                //aqui va 
-                //'marginLeft' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(3.5), 
-                //'marginRight' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5),
-                //'marginTop' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5), 
-                //'marginBottom' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5)
+                
                 $section = $documento->addSection(array('pageNumberingStart' => 1), DocumentoController::margenes());
                 $footer = $section->addFooter();
                 $footer->addTextRun(array('alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER))->addField('PAGE');
@@ -405,6 +401,7 @@ class DocumentoController extends Controller
                 if ($con->contenido != null) {
                     $section->addTextBreak(1);
                 }
+                //Insertar imágenes en capítulos
                 $resultado = preg_replace('/\<\s*img[^\\>]*(?<!\/)(?=\>)/', '$0/', $con->contenido);
                 $resultado  = preg_replace('/(?<=src=["\'])(https?:)?(\/\/)?[^\/]+\/?(?=\/)/', '.', $resultado );
                 \PhpOffice\PhpWord\Shared\Html::addHtml($section, $resultado, false, false);
@@ -413,6 +410,8 @@ class DocumentoController extends Controller
                 foreach($con->contenidoCapitulo2 as $con2){
                     $section->addTitle($capitulo->orden_capitulo.'.'.$numSegundo.'.'.$numTercero++.' '.$con2->subtema,3);
                     $section->addTextBreak(1);
+                    
+                    //Insertar imágenes en capítulos
                     $resultado = preg_replace('/\<\s*img[^\\>]*(?<!\/)(?=\>)/', '$0/', $con2->contenido);
                     $resultado  = preg_replace('/(?<=src=["\'])(https?:)?(\/\/)?[^\/]+\/?(?=\/)/', '.', $resultado );
                     \PhpOffice\PhpWord\Shared\Html::addHtml($section, $resultado, false, false);
@@ -421,6 +420,8 @@ class DocumentoController extends Controller
                 ++$numSegundo;   
             } else{
                 if ($con->contenido != "<p>null</p>") {
+
+                    //Insertar imágenes en capítulos
                     $resultado = preg_replace('/\<\s*img[^\\>]*(?<!\/)(?=\>)/', '$0/', $con->contenido);
                     $resultado  = preg_replace('/(?<=src=["\'])(https?:)?(\/\/)?[^\/]+\/?(?=\/)/', '.', $resultado );
                     \PhpOffice\PhpWord\Shared\Html::addHtml($section, $resultado , false, false);
@@ -436,48 +437,18 @@ class DocumentoController extends Controller
         if($documentoTodo == null){
             $section = $documento->addSection(DocumentoController::margenes());
         } else{
-            //aqui va
-            //'marginLeft' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5), 
-            //'marginRight' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5),
-            //'marginTop' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5), 
-            //'marginBottom' => \PhpOffice\PhpWord\Shared\Converter::cmToTwip(2.5)
+
             $section = $documento->addSection(array('pageNumberingStart' => 1), DocumentoController::margenes());
             $footer = $section->addFooter();
             $footer->addTextRun(array('alignment' => \PhpOffice\PhpWord\SimpleType\Jc::CENTER))->addField('PAGE', array('format' => 'roman'));
         }
         $section->addTitle(mb_strtoupper('Resumen'), 1);
         $section->addTextBreak(1);
+
+        //Insertar imágenes en capítulos
         $resultado = preg_replace('/\<\s*img[^\\>]*(?<!\/)(?=\>)/', '$0/', $resumen[0]->contenido);
         $resultado  = preg_replace('/(?<=src=["\'])(https?:)?(\/\/)?[^\/]+\/?(?=\/)/', '.', $resultado );
 
-        // $dom = new \DOMDocument();
-        // libxml_use_internal_errors(true);
-        // $dom->loadHTML($resultado,LIBXML_NOXMLDECL | LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
-        // $anchors = $dom -> getElementsByTagName('img');
-        // foreach ($anchors as $element) {
-        //     $styleat = $element->getAttribute("style");
-        //     if($styleat != ""){
-        //         $element->setAttribute( "width", substr($styleat,6,-1) );
-        //     }
-            
-        // }
-        // $anchors2 = $dom -> getElementsByTagName('figure');
-        // foreach ($anchors2 as $element) {
-        //     $imgs = $element->getElementsByTagName("img");
-        //     $styleat = $element->getAttribute("style");
-        //     foreach ($imgs as $element2) {
-                
-        //         if($styleat != ""){
-        //             $element2->setAttribute( "width", substr($styleat,6,-1) );
-        //         }
-                
-        //     }
-            
-        // }
-       
-        // $resultado = $dom->saveXML(null,LIBXML_NOXMLDECL);
-        // $resultado = preg_replace('/^(.*)/', '', $resultado );
-        // \Debugbar::info($resultado);
         \PhpOffice\PhpWord\Shared\Html::addHtml($section,  $resultado , false, false);
     }
     
--- resources/views/formulariosDoc/contenidoCapitulo.blade.php
+++ resources/views/formulariosDoc/contenidoCapitulo.blade.php
@@ -554,7 +554,10 @@
     </form>
 
     <script>
-         var formulario = document.getElementById("formulario");
+        
+        //MODIFICACION DEL FORM ANTES DE ENVIARLO (PARA TABLAS E IMÁGENES)
+        var formulario = document.getElementById("formulario");
+        
         formulario.onsubmit = function (){
             saveCapitulosPreForm();
         }
@@ -566,62 +569,60 @@
 
                 if(auxobject != null){
 
-
                    // let auxstring = auxobject.innerHTML;
                     let parser = new DOMParser();
                     let xmlDoc = parser.parseFromString(window.editor[i].getData(),"text/html");
                     window.editor[i].destroy().then(()=>{
+
                         var imgsElements = xmlDoc.getElementsByTagName("body")[0].getElementsByClassName("image_resized");
-                    var tableElements = xmlDoc.getElementsByTagName("body")[0].getElementsByTagName("table");
+                        var tableElements = xmlDoc.getElementsByTagName("body")[0].getElementsByTagName("table");
 
-                    for(let ele of imgsElements){
+                        for(let ele of imgsElements){
                     
-                        if(ele.tagName == "IMG"){
+                            if(ele.tagName == "IMG"){
                         
-                        var realvalue =  parseFloat(ele.style.width) * 0.75;
-                        ele.setAttribute("width",realvalue);
+                                var realvalue =  parseFloat(ele.style.width) * 0.75;
+                                ele.setAttribute("width",realvalue);
                         
+                            }
+
+                            if(ele.tagName == "FIGURE" && !ele.classList.contains("image-style-side")){
+                    
+                                var realvalue =  parseFloat(ele.style.width) * 0.75;
+                                ele.childNodes[0].setAttribute("width",realvalue);
+                                var margin = (8.5 - (realvalue/96))/2;
+                                ele.childNodes[0].style.float= "right";
+                        
+                            }
+
+                            if(ele.tagName == "FIGURE" && ele.classList.contains("image-style-side")){
+                    
+                                var realvalue =  parseFloat(ele.style.width) * 0.75;
+                                ele.childNodes[0].setAttribute("width",realvalue);
+                                var margin = (8.5 - (realvalue/96));
+                                ele.childNodes[0].setAttribute("marginLeft",margin);
+                                ele.style.float= "right";
+                            }    
                         }
-
-                        if(ele.tagName == "FIGURE" && !ele.classList.contains("image-style-side")){
-                    
-                        var realvalue =  parseFloat(ele.style.width) * 0.75;
-                        ele.childNodes[0].setAttribute("width",realvalue);
-                        var margin = (8.5 - (realvalue/96))/2;
-                        //ele.childNodes[0].setAttribute("marginLeft",margin);
-                        ele.childNodes[0].style.float= "right";
-                        
-                        }
-
-                        if(ele.tagName == "FIGURE" && ele.classList.contains("image-style-side")){
-                    
-                    var realvalue =  parseFloat(ele.style.width) * 0.75;
-                    ele.childNodes[0].setAttribute("width",realvalue);
-                    var margin = (8.5 - (realvalue/96));
-                    ele.childNodes[0].setAttribute("marginLeft",margin);
-                    ele.style.float= "right";
-                        }    
-                    }
 
             
-                    for(let ele of tableElements){
-                    ele.style.textAlign = "center";
-                    ele.style.padding = "10px";
-                    }
+                        for(let ele of tableElements){
+                            ele.style.textAlign = "center";
+                            ele.style.padding = "10px";
+                        }
 
-                    console.log(xmlDoc.getElementsByTagName("body")[0].innerHTML);
-                    let auxobject2 = document.getElementById("seccionTexto" + i);
-                    auxobject2.value = xmlDoc.getElementsByTagName("body")[0].innerHTML;
-                    //auxobject2.innerHTML = xmlDoc.getElementsByTagName("body")[0].innerHTML;
+                        console.log(xmlDoc.getElementsByTagName("body")[0].innerHTML);
+                        let auxobject2 = document.getElementById("seccionTexto" + i);
+                        auxobject2.value = xmlDoc.getElementsByTagName("body")[0].innerHTML;
+                        //auxobject2.innerHTML = xmlDoc.getElementsByTagName("body")[0].innerHTML;
                     
                     });
                    
-                    }
+                }
                
 
             }
            
-            //formulario.submit();
         }
 
     </script>
