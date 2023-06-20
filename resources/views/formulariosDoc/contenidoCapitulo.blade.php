4a36d5171578466113dac0aca360d02b53582754
Hanyauku123 <00353817@uca.edu.sv>
2023-06-19 21:48:35 -0600

    editor fix 1

--- resources/views/formulariosDoc/contenidoCapitulo.blade.php
+++ resources/views/formulariosDoc/contenidoCapitulo.blade.php
@@ -480,7 +480,7 @@
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </symbol>
     </svg>
-    <form action="{{ url('/contenidoCapitulo/guardarTemas') }}" id="formulario" method="POST">
+    <form action="{{ url('/contenidoCapitulo/guardarTemas') }}" id="formulario" method="POST" >
         {{ csrf_field() }}
         <div id="liveAlertPlaceholder"></div>
         <div class="accordion3" id="accordionExample34">
@@ -564,7 +564,7 @@
        
         function saveCapitulosPreForm(){
             
-            for(let i = 0; i < contadorGlobal; i++){
+            for(let i = 0; i <= contadorGlobal; i++){
                 let auxobject = document.getElementById("seccionTexto" + i);
 
                 if(auxobject != null){
@@ -580,7 +580,7 @@
                         for(let ele of imgsElements){
                     
                             if(ele.tagName == "IMG"){
-                        
+                                
                                 var realvalue =  parseFloat(ele.style.width) * 0.75;
                                 ele.setAttribute("width",realvalue);
                         
